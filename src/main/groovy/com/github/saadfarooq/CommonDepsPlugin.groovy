72104e0cbdf34a1466b2321fb5e9edbe847f307c
Saad Farooq <sa@dfarooq.com>
2016-10-23 22:33:24 -0400

    Initial commit

---
+++ src/main/groovy/com/github/saadfarooq/CommonDepsPlugin.groovy
@@ -0,0 +1,62 @@
+package com.github.saadfarooq
+
+import org.gradle.api.Plugin
+import org.gradle.api.Project
+import org.gradle.api.artifacts.DependencyResolutionListener
+import org.gradle.api.artifacts.ResolvableDependencies
+
+class CommonDepsPlugin implements Plugin<Project> {
+    def compileDeps
+    def logger
+    Project project
+
+    @Override
+    void apply(Project project) {
+        this.project = project
+        CommonDepsExtension commonDeps = project.extensions.create("commonDeps", CommonDepsExtension)
+        GoogleSupportDepsExtension support = project.commonDeps.extensions.create("support", GoogleSupportDepsExtension)
+        GooglePlayServicesExtension gps = project.commonDeps.extensions.create("gps", GooglePlayServicesExtension)
+        RxDepsExtension rx = project.commonDeps.extensions.create('rx', RxDepsExtension)
+        RxBindingDepsExtension rxBinding = rx.extensions.create('binding', RxBindingDepsExtension)
+
+        TestDepsExtension testing = project.commonDeps.extensions.create('testing', TestDepsExtension)
+//        AssertJDepsExtension assertj = testing.extensions.create('assertj', AssertJDepsExtension)
+
+        logger = project.logger
+        compileDeps = project.getConfigurations().getByName("compile").getDependencies()
+        project.getGradle().addListener(new DependencyResolutionListener() {
+            @Override
+            void beforeResolve(ResolvableDependencies resolvableDependencies) {
+                addDep(support)
+                addDep(commonDeps)
+                addDep(gps)
+                addDep(rx)
+                addDep(rxBinding)
+                addTestDeps(testing)
+//                addTestDeps(assertj)
+                project.getGradle().removeListener(this)
+            }
+
+            @Override
+            void afterResolve(ResolvableDependencies resolvableDependencies) {}
+        })
+    }
+
+    def addDep(support) {
+        support.getProperties().each { prop, val ->
+            if (val.getClass().equals(String.class) && val ==~/.*:*:.*/) {
+                logger.info "${prop}, ${val}"
+                compileDeps.add(project.getDependencies().create(val))
+            }
+        }
+    }
+
+    def addTestDeps(testing) {
+        def testDeps = project.getConfigurations().getByName("testCompile").getDependencies()
+        testing.getProperties().each { prop, value ->
+            if (value instanceof String && !value.isEmpty()) {
+                testDeps.add(project.getDependencies().create(value))
+            }
+        }
+    }
+}
---
+++ src/main/groovy/com/github/saadfarooq/DefaultVersions.groovy
@@ -0,0 +1,20 @@
+package com.github.saadfarooq
+
+class DefaultVersions {
+    static def ROBOLECTRIC = '3.0'
+    static def JUNIT = '4.12'
+    static def BUTTERKNIFE = '7.0.1'
+    static def PICASSO = '2.5.2'
+    static def TIMBER = '4.1.0'
+    static def SUPPORT_LIBS_VERSION = '23.1.0'
+    static def PLAY_SERVICES_LIBS_VERSION = '8.3.0'
+    static def GLIDE = '3.6.1'
+    static def RXANDROID = '1.0.1'
+    static def JODA_TIME_ANDROID = '2.9.0'
+    static def MOCKITO = '1.10.19'
+    static def RXLOCATION = '0.8@aar'
+    static def RXBINDING_VERSION = '0.3.0'
+    static def ASSERTJ_LIBS_VERSION = '1.1.1'
+    static def RXLIFECYCLE = '0.3.0'
+    static def RXPERMISSIONS = '0.4.2@aar'
+}

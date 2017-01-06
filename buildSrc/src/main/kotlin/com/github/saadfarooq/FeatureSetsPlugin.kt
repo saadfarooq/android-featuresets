7fd08a5575517e8bfb5d75a36383f988a53d9b22
Saad Farooq <sa@dfarooq.com>
2017-01-06 01:05:04 -0500

    Progress

--- buildSrc/src/main/kotlin/com/github/saadfarooq/FeatureSetsPlugin.kt
+++ buildSrc/src/main/kotlin/com/github/saadfarooq/FeatureSetsPlugin.kt
@@ -3,14 +3,13 @@ package com.github.saadfarooq;
 import com.android.build.gradle.AppExtension
 import com.android.build.gradle.AppPlugin
 import com.android.build.gradle.api.AndroidSourceSet
-import com.android.build.gradle.tasks.factory.AndroidJavaCompile
 import com.github.saadfarooq.model.FeatureSet
 import org.atteo.xmlcombiner.XmlCombiner
 import org.gradle.api.NamedDomainObjectCollection
 import org.gradle.api.Plugin
 import org.gradle.api.Project
-import org.gradle.api.Task
 import org.gradle.api.logging.Logger
+import org.w3c.dom.Element
 import java.io.File
 
 class FeatureSetsPlugin : Plugin<Project> {
@@ -50,7 +49,12 @@ class FeatureSetsPlugin : Plugin<Project> {
                         androidExtension.sourceSets.getByName(featureSet.name) // debug, main, release
                                 ?.let { srcSet ->
                                     featureSet.features.forEach { srcSet.addSrcFolders("src/$it") }
-                                    mergeManifests(project, srcSet, featureSet.features)
+                                    LOGGER.info("Creating merge${featureSet.name.capitalize()}FeatureManifest task $srcSet")
+                                    val srcManifest = project.buildDir.resolve("intermediates/manifests/featureSets/${srcSet.name}/AndroidManifest.xml")
+                                    project.tasks.create("merge${featureSet.name.capitalize()}FeatureManifest").doLast {
+                                        mergeManifests(project, srcSet, featureSet.features, srcManifest, srcSet.manifest.srcFile)
+                                    }
+                                    srcSet.manifest.srcFile(srcManifest)
                                 }
                         val testSet = if (featureSet.name == "main") "test" else "test${featureSet.name.capitalize()}"
                         androidExtension.sourceSets.findByName(testSet)
@@ -62,29 +66,50 @@ class FeatureSetsPlugin : Plugin<Project> {
             androidExtension.applicationVariants.forEach { variant ->
                 val files = variant.sourceSets.map { it.javaDirectories }
                         .reduce { acc, i -> acc.plus(i) }
-                (variant.javaCompiler as AndroidJavaCompile).source(files)
+                val dummyGeneratingTask = project.task("register${variant.name.capitalize()}FeatureSetSrcs")
+                variant.registerJavaGeneratingTask(dummyGeneratingTask, files)
+                featureSets.map { project.getTasksByName("merge${it.name.capitalize()}FeatureManifest", false) }
+                        .flatten()
+                        .forEach {
+                            LOGGER.info("Adding $it to variant: ${variant.name}")
+                            variant.checkManifest.dependsOn(it)
+                            variant.checkManifest.mustRunAfter(it)
+                        }
             }
         }
     }
 
-    private fun mergeManifests(project: Project, srcSet: AndroidSourceSet, features: Set<String>) {
-        project.tasks.create("processFeatureManifest", FeatureManifestTask::class.java)
+    private fun mergeManifests(project: Project, srcSet: AndroidSourceSet, features: Set<String>, srcManifest: File, originalManifest: File) {
+        println("-------- >>>>> writing manifest for ${srcSet.name}, $features")
         features.map { project.file("src/$it/AndroidManifest.xml") }
-                .plus(srcSet.manifest.srcFile)
+                .plus(project.file("src/${srcSet.name}/AndroidManifest.xml"))
                 .filter(File::exists)
-                .let { if (it.isNotEmpty())
-                    it.fold(XmlCombiner(), { xmlCombiner, manifestFile -> xmlCombiner.let { it.combine(manifestFile.toPath()); it }})
+                .let { if (it.isNotEmpty()) {
+                    val xmlCombiner = XmlCombiner()
+                    xmlCombiner.setFilter(XmlCombiner.Filter(fn))
+                    it.fold(xmlCombiner, { xc, manifestFile -> xc.let { it.combine(manifestFile.toPath()); it }})
                             .let {
-                                val srcManifest = project.buildDir.resolve("intermediates/manifests/featureSets/${srcSet.name}/AndroidManifest.xml")
                                 LOGGER.info("Writing featureSet manifest to ${srcManifest.path}")
                                 it.buildDocument(
                                         srcManifest
                                                 .apply { parentFile.mkdirs(); createNewFile() }
                                                 .outputStream())
-                                srcSet.manifest.srcFile(srcManifest)
                             }
                 }
+                }
     }
+
+    val fn : (e1: Element?, e2: Element?, e3: Element?) -> Unit = {
+        e1, e2, e3 -> tempmethod(e1, e2, e3)
+    }
+
+    private fun tempmethod(e1: Element?, e2: Element?, e3: Element?) : Unit {
+        val node1 = e1?.childNodes?.item(0)
+        val node2 = e2?.childNodes?.item(0)
+        e3?.appendChild(node1)
+        e3?.appendChild(node2)
+    }
+
 
     fun AndroidSourceSet.addSrcFolders(folder: String) {
         this.java.srcDir("$folder/java")

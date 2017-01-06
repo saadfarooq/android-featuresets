958beea53b948101c8383a94075bedd2358b8468
Saad Farooq <sa@dfarooq.com>
2017-01-05 21:24:46 -0500

    progress

--- buildSrc/src/main/kotlin/com/github/saadfarooq/FeatureSetsPlugin.kt
+++ buildSrc/src/main/kotlin/com/github/saadfarooq/FeatureSetsPlugin.kt
@@ -3,13 +3,11 @@ package com.github.saadfarooq;
 import com.android.build.gradle.AppExtension
 import com.android.build.gradle.AppPlugin
 import com.android.build.gradle.api.AndroidSourceSet
-import com.android.build.gradle.tasks.factory.AndroidJavaCompile
 import com.github.saadfarooq.model.FeatureSet
 import org.atteo.xmlcombiner.XmlCombiner
 import org.gradle.api.NamedDomainObjectCollection
 import org.gradle.api.Plugin
 import org.gradle.api.Project
-import org.gradle.api.Task
 import org.gradle.api.logging.Logger
 import java.io.File
 
@@ -32,43 +30,56 @@ class FeatureSetsPlugin : Plugin<Project> {
     @Suppress("UNCHECKED_CAST")
     private fun processSourceSets(project: Project, androidExtension: AppExtension) {
         project.afterEvaluate {
-            val featureSets = project.extensions.getByName("featureSets") as NamedDomainObjectCollection<FeatureSet>
-            featureSets
-                    .apply {
-                        map { it.name }
-                                .filter { it != "main" } // main buildtypes always exists even if not defined
-                                .let {
-                                    val diff = it.minus(androidExtension.buildTypes.map { it.name })
-                                    if (diff.isNotEmpty()) {
-                                        throw IllegalArgumentException("Feature sets must match up to build types, " +
-                                                "following feature sets don't: $diff")
-                                    }
-                                }
-                    }.filter { it.features.isNotEmpty() }
-                    .forEach { featureSet ->
-                        println("processing: ${featureSet.name}")
-                        androidExtension.sourceSets.getByName(featureSet.name) // debug, main, release
-                                ?.let { srcSet ->
-                                    featureSet.features.forEach { srcSet.addSrcFolders("src/$it") }
-                                    mergeManifests(project, srcSet, featureSet.features)
-                                }
-                        val testSet = if (featureSet.name == "main") "test" else "test${featureSet.name.capitalize()}"
-                        androidExtension.sourceSets.findByName(testSet)
-                                ?.let { srcSet ->
-                                    featureSet.features.forEach { srcSet.addSrcFolders("src/$it/test") }
-                                }
-                    }
+            androidExtension.applicationVariants.all { variant ->
+                val featureSetDir = "${project.buildDir}/intermediates/manifests/featureSets"
+                val featureSets = project.extensions.getByName("featureSets") as NamedDomainObjectCollection<FeatureSet>
 
-            androidExtension.applicationVariants.forEach { variant ->
-                val files = variant.sourceSets.map { it.javaDirectories }
-                        .reduce { acc, i -> acc.plus(i) }
-                (variant.javaCompiler as AndroidJavaCompile).source(files)
+                featureSets
+                        .apply {
+                            map { it.name }
+                                    .filter { it != "main" } // main buildtypes always exists even if not defined
+                                    .let {
+                                        val diff = it.minus(androidExtension.buildTypes.map { it.name })
+                                        if (diff.isNotEmpty()) {
+                                            throw IllegalArgumentException("Feature sets must match up to build types, " +
+                                                    "following feature sets don't: $diff")
+                                        }
+                                    }
+                        }.filter { it.features.isNotEmpty() }
+                        .forEach { featureSet ->
+                            variant.sourceSets
+                                    .filter { it.name == featureSet.name }
+                                    .forEach { srcProvider ->
+                                        println("--- Before --------> $srcProvider, ${srcProvider.javaDirectories}")
+                                        featureSet.features.forEach { srcProvider.javaDirectories.add(project.file("src/$it/java")) }
+                                        println("--- After  --------> $srcProvider, ${srcProvider.javaDirectories}")
+                                    }
+
+//                            variant.sourceSets.getByName(featureSet.name) // debug, main, release
+//                                    ?.let { srcSet ->
+//                                        featureSet.features.forEach { srcSet.addSrcFolders("src/$it") }
+//                                        mergeManifests(project, srcSet, featureSet.features)
+//                                    }
+//                            val testSet = if (featureSet.name == "main") "test" else "test${featureSet.name.capitalize()}"
+//                            androidExtension.sourceSets.findByName(testSet)
+//                                    ?.let { srcSet ->
+//                                        featureSet.features.forEach { srcSet.addSrcFolders("src/$it/test") }
+//                                    }
+                        }
+
+//                androidExtension.applicationVariants.forEach { variant ->
+//                    val files = variant.sourceSets.map { it.javaDirectories }
+//                            .reduce { acc, i -> acc.plus(i) }
+//                    (variant.javaCompiler as AndroidJavaCompile).source(files)
+//                }
+
             }
+
         }
     }
 
     private fun mergeManifests(project: Project, srcSet: AndroidSourceSet, features: Set<String>) {
-        project.tasks.create("processFeatureManifest", FeatureManifestTask::class.java)
+//        project.tasks.create("processFeatureManifest", FeatureManifestTask::class.java)
         features.map { project.file("src/$it/AndroidManifest.xml") }
                 .plus(srcSet.manifest.srcFile)
                 .filter(File::exists)
